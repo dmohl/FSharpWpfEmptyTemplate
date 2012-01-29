5d106fbb703c71f53609d29a47d537f3817859be
Dan Mohl <dmohl@yahoo.com>
2012-01-29 09:59:10 -0600

    Created.

---
+++ src/App/App.fs
@@ -0,0 +1,15 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+// Create the View and bind it to the View Model
+let mainWindow = Application.LoadComponent(
+                             new System.Uri("/App;component/mainwindow.xaml", UriKind.Relative)) :?> Window
+
+// Application Entry point
+[<STAThread>]
+[<EntryPoint>]
+let main(_) = (new Application()).Run(mainWindow)
