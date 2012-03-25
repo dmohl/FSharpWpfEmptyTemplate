fa3f09c7fe6d67e7f9e491baccf0728a8dbff57b
Dan Mohl <dmohl@yahoo.com>
2012-03-25 15:46:41 -0500

    Upgraded the FSharpx.TypeProviders NuGet package to version 1.5.3

--- FsWpfEmptyTemplateWizard/TemplateWizard.fs
+++ FsWpfEmptyTemplateWizard/TemplateWizard.fs
@@ -55,7 +55,7 @@ type TemplateWizard() =
                         try
                             this.dte2.StatusBar.Text <- "Adding NuGet packages..."
                             (projects.TryFind this.safeProjectName).Value |> InstallPackages this.serviceProvider (templatePath.Replace(templateName + ".vstemplate", ""))
-                            <| [("FSharpx.TypeProviders", "1.5.1")]
+                            <| [("FSharpx.TypeProviders", "1.5.3")]
                         with
                         | ex -> let msg = ex
                                 ()// do nothing...
